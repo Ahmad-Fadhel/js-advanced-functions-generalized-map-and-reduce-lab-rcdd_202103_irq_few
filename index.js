faea64d554cdb01388a9f7e1dd52b76067a4140a
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-05-18 23:14:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+const map = (sourceArray, fn)=>{
+  let newArray = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    let element = sourceArray[i]
+    newArray.push(fn(element))
+  }
+  return newArray
+};
+
+const reduce = (sourceArray, fn, start)=>{
+  if (start) {
+      let total = start
+      for (let i = 0; i < sourceArray.length; i++) {
+        let element = sourceArray[i]
+        total = fn(total, element)
+      }
+      return total
+  }
+  else {
+      let total = sourceArray[0]
+      for (let i = 1; i < sourceArray.length; i++) {
+        let element = sourceArray[i]
+        total = fn(total, element)
+      }
+      return total
+  }
+
+};
+
